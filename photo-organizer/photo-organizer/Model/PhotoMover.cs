c8eb636af8b0c89c968b67fbb62c0e705f61d5ae
Richard Kuperus <richard.kuperus@outlook.com>
2017-01-14 17:10:40 +0100

    First working version. Fixed a random crash when photo file could not be opened

--- photo-organizer/photo-organizer/Model/PhotoMover.cs
+++ photo-organizer/photo-organizer/Model/PhotoMover.cs
@@ -4,18 +4,21 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Services.Maps;
 using Windows.Storage;
 
 namespace photo_organizer.Model
 {
     public class PhotoMover : INotifyPropertyChanged
     {
-
-        #region Observable properties
+        private const string SINKHOLE_FOLDER_NAME = "\\unknown";
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private StorageFile lastImageLocation = null;
+        #region Observable properties
+
+        public StorageFile LastImageLocation = null;
 
         private string _currentImage;
         public string CurrentImage
@@ -25,39 +28,6 @@ namespace photo_organizer.Model
             {
                 _currentImage = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentImage"));
-            }
-        }
-
-        private string _duration;
-        public string Duration
-        {
-            get { return _duration; }
-            set
-            {
-                _duration = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Duration"));
-            }
-        }
-
-        private string _pps;
-        public string PPS
-        {
-            get { return _pps; }
-            set
-            {
-                _pps = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PPS"));
-            }
-        }
-
-        private string _ppslm;
-        public string PPSLM
-        {
-            get { return _ppslm; }
-            set
-            {
-                _ppslm = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PPSLM"));
             }
         }
 
@@ -186,9 +156,222 @@ namespace photo_organizer.Model
 
             CurrentFolderName = sourceFolder.Name;
 
-            // await ProcessFiles(sourceFolder, destinationFolder, moveFiles);
+            await ProcessFiles(sourceFolder, destinationFolder, moveFiles);
 
             return true;
+        }
+
+        async private Task<bool> ProcessFiles(StorageFolder sourceFolder, StorageFolder destinationRootFolder, bool moveFiles)
+        {
+            AddToStream($"Retrieving files from folder {sourceFolder.Name}");
+            var files = await sourceFolder.GetFilesAsync();
+            foreach (var file in files)
+            {
+                CurrentImage = file.DisplayName;
+                AddToStream($"Processing file {file.Name} of type {file.ContentType}");
+
+                if (string.Compare(file.ContentType, "image/jpeg", true) == 0)
+                {
+                    await ProcessImageFile(file, destinationRootFolder, moveFiles);
+                }
+                else if (string.Compare(file.ContentType, "video/avi", true) == 0 || string.Compare(file.ContentType, "video/mp4", true) == 0 || string.Compare(file.ContentType, "video/quicktime", true) == 0 || file.ContentType.StartsWith("video/"))
+                {
+                    await ProcessVideoFile(file, sourceFolder, destinationRootFolder, moveFiles);
+                }
+
+                PhotoCount++;
+
+                if (CancelExecute) return false;
+            }
+
+            return true;
+        }
+
+        async private Task ProcessVideoFile(StorageFile file, StorageFolder sourceFolder, StorageFolder destinationRootFolder, bool moveFiles)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (file.Properties != null)
+            {
+
+                var fileProperties = file.Properties;
+                var videoProperties = await fileProperties.GetVideoPropertiesAsync();
+                if (null != videoProperties)
+                {
+                    if (videoProperties.Year > 0)
+                    {
+                        sb.Append($"\\{ videoProperties.Year }\\Video");
+                    }
+                    else
+                    {
+                        VideoPathNameAppend(sourceFolder, sb);
+                    }
+
+                    if (null != videoProperties.Latitude && null != videoProperties.Longitude)
+                    {
+                        Geopoint p = new Geopoint(new BasicGeoposition
+                        {
+                            Latitude = videoProperties.Latitude.Value,
+                            Longitude = videoProperties.Longitude.Value
+                        });
+
+                        MapLocationFinderResult location = await MapLocationFinder.FindLocationsAtAsync(p);
+                        var l = location.Locations.FirstOrDefault<MapLocation>();
+                        if (null != l)
+                        {
+                            if (!string.IsNullOrWhiteSpace(l.DisplayName))
+                            {
+                                sb.Append($"\\{l.DisplayName}");
+                            }
+                            else
+                            {
+                                if (null != l.Address && null != l.Address.Town)
+                                {
+                                    sb.Append($"\\{l.Address.Town}");
+                                }
+                            }
+                        }
+                    }
+
+                    MoveOrCopyFileTo(file, destinationRootFolder, sb.ToString(), moveFiles);
+                    return;
+                }
+            }
+
+            // missing information, we'll use current path information instead
+            VideoPathNameAppend(sourceFolder, sb);
+            MoveOrCopyFileTo(file, destinationRootFolder, sb.ToString(), moveFiles);
+        }
+
+        private static void VideoPathNameAppend(StorageFolder sourceFolder, StringBuilder sb)
+        {
+            var splitName = sourceFolder.Name.Split('-');
+            if (splitName.Length == 2)
+            {
+                var year = splitName[0];
+                var month = splitName[1];
+
+                sb.Append($"\\{year}");
+                sb.Append($"\\{month:00}-{year:0000}");
+                sb.Append("\\Video");
+            }
+            else
+            {
+                sb.Append(sourceFolder.Name.Replace('-', '\\'));
+                sb.Append("\\Video");
+            }
+        }
+
+        async private Task ProcessImageFile(StorageFile file, StorageFolder destinationRootFolder, bool moveFiles)
+        {
+            if (null != file.Properties)
+            {
+                var fileProperties = file.Properties;
+                var imageProperties = await fileProperties.GetImagePropertiesAsync();
+                if (null != imageProperties)
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    sb.Append($"\\{imageProperties.DateTaken.Year:0000}");
+                    sb.Append($"\\{imageProperties.DateTaken.Month:00}-{imageProperties.DateTaken.Year:0000}");
+
+                    if (null != imageProperties.Latitude && null != imageProperties.Longitude)
+                    {
+                        Geopoint p = new Geopoint(new BasicGeoposition
+                        {
+                            Latitude = imageProperties.Latitude.Value,
+                            Longitude = imageProperties.Longitude.Value
+                        });
+
+                        MapLocationFinderResult location = await MapLocationFinder.FindLocationsAtAsync(p);
+                        var l = location.Locations.FirstOrDefault<MapLocation>();
+                        if (null != l)
+                        {
+                            if (!string.IsNullOrWhiteSpace(l.DisplayName))
+                            {
+                                sb.Append($"\\{l.DisplayName}");
+                            }
+                            else
+                            {
+                                if (null != l.Address && null != l.Address.Town)
+                                {
+                                    sb.Append($"\\{l.Address.Town}");
+                                }
+                            }
+                        }
+                    }
+                    else if ( !string.IsNullOrWhiteSpace(imageProperties.CameraModel) )
+                    {
+                        sb.Append($"\\{imageProperties.CameraManufacturer} -  {imageProperties.CameraModel}");
+                    }
+
+                    MoveOrCopyFileTo(file, destinationRootFolder, sb.ToString(), moveFiles);
+
+                    LastImageLocation = file;
+                    return;
+                }
+            }
+
+            // MoveOrCopyFileTo(file, destinationRootFolder, SINKHOLE_FOLDER_NAME, moveFiles);
+        }
+
+        async private Task<StorageFolder> CreateFolderTreeAsync(StorageFolder root, string newPath)
+        {
+            StorageFolder f = root;
+            if (!string.IsNullOrWhiteSpace(newPath))
+            {
+                var pathElements = newPath.Split(new char[] { '\\' });
+                foreach (var e in pathElements)
+                {
+                    if (!string.IsNullOrWhiteSpace(e))
+                        f = await f.CreateFolderAsync(e, CreationCollisionOption.OpenIfExists);
+                }
+            }
+
+            return f;
+        }
+
+        async private void MoveOrCopyFileTo(StorageFile file, StorageFolder folder, string destinationPath, bool moveFiles)
+        {
+            var destination = await CreateFolderTreeAsync(folder, destinationPath);
+            if (!await FileExistsAsync(destination, file.Name))
+            {
+                try
+                {
+                    if (moveFiles)
+                    {
+                        await file.MoveAsync(destination, file.Name, NameCollisionOption.GenerateUniqueName);
+                    }
+                    else
+                    {
+                        await file.CopyAsync(destination, file.Name, NameCollisionOption.GenerateUniqueName);
+                    }
+
+                    AddToStream(file.DisplayName);
+                }
+                catch (Exception e)
+                {
+                    AddToStream($"Error with message {e.Message}");
+                    ErrorCount++;
+                }
+            }
+            else
+            {
+                Duplicates += 1;
+            }
+        }
+
+        async private Task<bool> FileExistsAsync(StorageFolder destination, string name)
+        {
+            bool exists = true;
+            try
+            {
+                await destination.GetFileAsync(name);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                exists = false;
+            }
+            return exists;
         }
     }
 }
--- photo-organizer/photo-organizer/OrganizeProgress.xaml.cs
+++ photo-organizer/photo-organizer/OrganizeProgress.xaml.cs
@@ -13,24 +13,71 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using photo_organizer.Model;
+using System.ComponentModel;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Popups;
 
 namespace photo_organizer
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class OrganizeProgress : Page
+    public sealed partial class OrganizeProgress : Page, INotifyPropertyChanged
     {
-        Model.PhotoMover Mover = new Model.PhotoMover();
-        Model.Configuration configurationSettings;
+        public PhotoMover Mover = new PhotoMover();
+        Configuration configurationSettings;
         DispatcherTimer timer;
         DateTime startTime;
         int currentSecond;
 
+        private string _duration;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string Duration
         {
-            get;set;
+            get { return _duration; }
+            set
+            {
+                _duration = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Duration"));
+            }
+        }
+
+        private string _photosPerSecond;
+        public string PhotosPerSecond
+        {
+            get { return _photosPerSecond; }
+            set
+            {
+                _photosPerSecond = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PhotosPerSecond"));
+            }
+        }
+
+        private string _photosInFolders;
+        public string PhotosInFolders
+        {
+            get { return _photosInFolders; }
+            set
+            {
+                _photosInFolders = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PhotosInFolders"));
+            }
+        }
+
+
+        private double _maxPhotosPerSecond = 0;
+        public double MaxPhotosPerSecond
+        {
+            get { return _maxPhotosPerSecond; }
+            set
+            {
+                _maxPhotosPerSecond = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MaxPhotosPerSecond"));
+            }
         }
 
         public OrganizeProgress()
@@ -52,6 +99,8 @@ namespace photo_organizer
             {
                 AddToStream("About to start photo organization complete");
 
+                base.DataContext = Mover;
+
                 timer = new DispatcherTimer();
                 timer.Interval = TimeSpan.FromSeconds(1);
                 timer.Tick += Timer_Tick;
@@ -61,9 +110,13 @@ namespace photo_organizer
 
                 timer.Start();
 
+                RestartButton.IsEnabled = false;
+
                 await Mover.Execute(configurationSettings.SourceFolder, configurationSettings.DestinationFolder, configurationSettings.MoveFilesToDestination);
 
                 timer.Stop();
+
+                await new MessageDialog("Completed").ShowAsync();
   
                 AddToStream("Photo organization complete");
             }
@@ -73,9 +126,56 @@ namespace photo_organizer
             }
         }
 
-        private void Timer_Tick(object sender, object e)
+        private async void Timer_Tick(object sender, object e)
         {
-            throw new NotImplementedException();
+            DateTime tick = DateTime.Now;
+            currentSecond = tick.Second;
+
+            var tsDuration = tick - startTime;
+            Duration = tsDuration.ToString("%m' min, '%s' sec.'");
+
+            var photosPerSecond = Mover.PhotoCount / (double)tsDuration.TotalSeconds;
+            MaxPhotosPerSecond = System.Math.Max(photosPerSecond, MaxPhotosPerSecond);
+
+            PhotosPerSecond = string.Format("{0} ({1})", photosPerSecond.ToString("N2"), MaxPhotosPerSecond.ToString("N2"));
+
+            PhotosPerSecondBar.Maximum = MaxPhotosPerSecond;
+            PhotosPerSecondBar.Value = photosPerSecond;
+
+            PhotosInFolders = string.Format("{0} photo's in {1} folders", Mover.PhotoCount, Mover.FolderCount);
+
+            if (null != Mover.LastImageLocation)
+                await ChangePreviewImage();
+        }
+
+        private async System.Threading.Tasks.Task ChangePreviewImage()
+        {
+            try
+            {
+                using (IRandomAccessStream fileStream = await Mover.LastImageLocation.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+
+                    await bitmapImage.SetSourceAsync(fileStream);
+                    LastProcessedImage.Source = bitmapImage;
+                }
+            }
+            catch (Exception ex)
+            {
+                AddToStream(ex.Message);
+            }
+        }
+
+        private void PauzeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Mover.CancelExecute = true;
+            RestartButton.IsEnabled = true;
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            Mover.CancelExecute = true;
+            this.Frame.Navigate(typeof(ConfigureOrganize));
         }
     }
 }
